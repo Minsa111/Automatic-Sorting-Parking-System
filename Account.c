517a9ce3e08514525af44980e8b72d324a5df071
Minsa111 <dragonriderrgt@gmail.com>
2022-01-03 17:45:36 +0700

    sementara

--- Account.c
+++ Account.c
@@ -5,7 +5,7 @@
 #include <conio.h>
 
 
-int db_person=0;
+double db_person=0;
 char pil;
 char acc[99];
 char username['0'][99], password['0'][99]; plat['0'][99];
@@ -13,6 +13,7 @@ char input_plat[99], input_user[99], input_pass[99];
 
 bool user_allow = false;
 bool exist=false;
+int compare_user, compare_plat, compare_pass;
 int i ,j ,k ,count;
 
 
@@ -24,6 +25,11 @@ int main(){
     switch(getch()){
         case'1': 
             system("cls");
+            if (db_person == 20){
+                printf("Sorry there's no parking slot available");
+                system ("pause");
+                goto home;
+            }
             reg:
             exist = false;
             j=0;
@@ -32,14 +38,15 @@ int main(){
             printf("\n\t== Register == ");
             printf("\n============================ \n");
             printf("\nEnter Username\t\t\t: "); scanf("%s", input_user);
-            for(i=1; i<=db_person ; i++){
-                if(strcmp(input_user, username[i])==0){
-                    exist=true;
-                    break;
-                }
-            }
-                if(exist==true){
 
+            // nyari kalo username udah ada apa belum.
+                for(i=1; i<=db_person ; i++){
+                    if(strcmp(input_user, username[i])==0){
+                        exist=true;
+                        break;
+                    }
+                }
+                if(exist==true){
                     printf("=== Sorry, username is not available ===");
                     printf("\n\nDo you want to try again?(y/n)");
                     switch(getch()){
@@ -49,63 +56,40 @@ int main(){
                         break;
                         case'n':
                         case'N':
+                            db_person--;
                             goto home;
                         break;
                     }
                 }
-            plat_input:
-            exist = false;
-            printf("\nEnter Car Plat number\t\t:"); scanf("%[^\n]s", input_plat);
+                reg_plat:
+                printf("Enter car plat number!");
+                scanf("%s", input_plat);
                 strupr(input_plat);
-                for(i=0 ; i<db_person ; i++){
-                    if (strcmp(input_plat, plat[i])==0){
-                        exist = true;
-                        break;
-                    }
-                }
-                if (exist==true)
-                {
-                    printf("===Plat number is not available===\n");
-                    printf("Do you wish to exit from creating data?(y/n)");
-                        switch(getch()){
-                            case 'Y':
-                            case 'y':
-                                goto home;
-                            break;
-                            case 'N':
-                            case 'n':
-                                goto plat_input;
-                            break;
-                            default:
-                        printf("\n\nPlease enter a valid answer!");
-                        system("pause");
-                    goto reg;
+                    count = strlen(input_plat);
+                        for(i=0 ; i<=count ; i++){
+                            if(!(input_plat[i] >='A' && input_plat[i] <='Z') && !(input_plat[i]>='0' && input_plat[i]<='9') && !(input_plat[i]=='\0')){
+                                    j++;
+                                    break;
+                            }
                         }
-                }
-                count = strlen(input_plat);
-                    for(i=0 ; i<=count ; i++){
-                        if(!(input_plat[i] >='A' && input_plat[i] <='Z') && !(input_plat[i]>='0' && input_plat[i]<='9') && !(input_plat[i]=='\0')){
-                                j++;
+                    if(j==1){
+                        printf("===The plat should not contain any special characters!===");
+                        printf("Do you wish to exit from creating data?(y/n)");
+                            switch(getch()){
+                                case 'Y':
+                                case 'y':
+                                db_person--;
+                                    goto home;
                                 break;
-                        }
-                    }
-                if(j==1){
-                    printf("===The plat should not contain any special characters!===");
-                    printf("Do you wish to exit from creating data?(y/n)");
-                        switch(getch()){
-                            case 'Y':
-                            case 'y':
-                                goto home;
-                            break;
-                            case 'N':
-                            case 'n':
-                                goto plat_input;
-                            break;
-                            default:
-                        printf("\n\nPlease enter a valid answer!");
-                        system("pause");
-                    goto reg;
-                }}
+                                case 'N':
+                                case 'n':
+                                    goto reg_plat;
+                                break;
+                                default:
+                            printf("\n\nPlease enter a valid answer!");
+                            system("pause");
+                        goto reg;
+                    }}
             printf("\nEnter Pass\t\t\t: "); scanf("%s", input_pass);
 
             printf("\n========================== \n");
